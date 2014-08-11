fb7bf08f17a75e6495cff60806e0650af3e17966
Oldřich Hobl <oldrich.hobl@gmail.com>
2014-08-11 18:13:32 +0200

    Nahrany zaklad
    Nahrany zaklad

---
+++ install.bat
@@ -0,0 +1,8 @@
+rem
+set ANDROID_HOME=c:\AndroidSDK\sdk
+set ANT_HOME=c:\ANT
+set JAVA_HOME=c:\Program Files\Java\jdk1.7.0_51
+
+PATH=%PATH%;%ANT_HOME%\bin;%ANDROID_HOME%\tools;%ANDROID_HOME%\platform-tools;%JAVA?HOME%\bin
+
+
---
+++ start.bat
@@ -0,0 +1,3 @@
+rem
+"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security --allow-file-access-from-files file://C:/_VYVOJ/VYVOJ-FileAPI/CLI/www/index.html
+
